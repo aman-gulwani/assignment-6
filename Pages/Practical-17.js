20beeee569c1de33701131e43ff37ca9b243d397
Aman Gulwani <aman.gulwani_cs18@gla.ac.in>
2020-11-09 02:47:31 +0530

    image

--- Pages/Practical-17.js
+++ Pages/Practical-17.js
@@ -28,11 +28,39 @@ function addZero(n) {
 }
 
 function setGreeting() {
-    let today = new Date();
-    let hour = today.getHours();
+    let today = new Date(2020, 12, 10, 22, 8, 8);
+    let hours = today.getHours();
 
-    if (hour > 0 && hour < 4) {
-        document.body.style.backgroundImage = "url('/image/night3.webp')";
+    if (hours >= 0 && hours < 5) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/night3.webp')";
+        greeting.innerHTML = "Good Night";
+        document.body.style.color  = "white";
+    }
+
+    else if (hours > 4 && hours < 8) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/early_morning.jpg')";
+        greeting.innerHTML = "Good Morning";
+    }
+
+    else if (hours > 7 && hours < 13) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/morning1.webp')";
+        greeting.innerHTML = "Good Morning";
+    }
+
+    else if (hours > 12 && hours < 17) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/afternoon1.jpg')";
+        greeting.innerHTML = "Good Afternoon";
+    }
+
+    else if (hours > 16 && hours < 20) {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/evening.webp')";
+        greeting.innerHTML = "Good Evening";
+    }
+
+    else {
+        document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/aman-gulwani/assignment-6/master/Image/night4.webp')";
+        greeting.innerHTML = "Good Night";
+        document.body.style.color  = "white";
     }
 }
 
